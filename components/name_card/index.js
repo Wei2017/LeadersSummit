6d11b5bc56deecb48cb39868fdc9d1be475a1241
杜晓伟 <527188405@qq.com>
2018-10-31 18:49:42 +0800

    components add name_card&update guest_details

---
+++ components/name_card/index.js
@@ -0,0 +1,28 @@
+// components/name_card/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    userPic:String,
+    userName:String,
+    userPhone:Number,
+    userEmail:String,
+    company:String,
+    position:String
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
